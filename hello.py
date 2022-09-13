c57552b14b63fda0dd081efa54fd064c642803a5
Alexander Sosnovsky <106810534+LeshenShow@users.noreply.github.com>
2022-09-13 11:52:54 +0300

    Add hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+name = 'gus'
+print(name)
+
+# cmd \\ terminal write:
+# cd Name \\ or cd "C:\My projects\plots\animation"  - заходим в данную папку
+# dir - что есть в папке
+# git clone https://github.com/LeshenShow/git_tut.git -клонируем репозиторий в папку
+
+# Потом заходим в папку с проекторм cd C:\My projects\Туториалы\Git_tuts\git_test\git_tut
+# Добавляем новый наш файл в репозиторий C:\My projects\Туториалы\Git_tuts\git_test\git_tut>git add hello.py
